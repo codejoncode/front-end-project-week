aefa520656c1542b0e4c7635aeb3699744080b20
codejoncode <jonathanjholloway@gmail.com>
2019-03-02 19:01:57 -0600

    Updates

---
+++ precache-manifest.f44f20f327553635e9e78537cab88ebd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f02b747d345354bdf597f315280e4df3",
+    "url": "/front-end-project-week/static/media/trash.f02b747d.jpg"
+  },
+  {
+    "revision": "74c9633621124cc59c84",
+    "url": "/front-end-project-week/static/js/runtime~main.74c96336.js"
+  },
+  {
+    "revision": "9b38bf6cb72a3f163cbd",
+    "url": "/front-end-project-week/static/js/main.9b38bf6c.chunk.js"
+  },
+  {
+    "revision": "cf58ca441541c0461a8f",
+    "url": "/front-end-project-week/static/js/2.cf58ca44.chunk.js"
+  },
+  {
+    "revision": "9b38bf6cb72a3f163cbd",
+    "url": "/front-end-project-week/static/css/main.f6402283.chunk.css"
+  },
+  {
+    "revision": "2ba635b2ff3a4edce037be6d004ba27b",
+    "url": "/front-end-project-week/index.html"
+  }
+];
